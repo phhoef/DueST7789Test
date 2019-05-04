df75434ecf70a9df59a82f104365eaf87d15d6e4
ph <>
2019-05-04 19:56:36 +0200

    ST7789 working

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,22 @@
 
 void setup()
 {
-    pinMode(25, OUTPUT);
-    digitalWrite(25, HIGH);
+    pinMode(PIN_POWER, OUTPUT);
+    digitalWrite(PIN_POWER, HIGH);
+
+    Serial.begin(9600);
+    Serial.println("Starting ...");
+
+    pinMode(BUTTON_LEFT_DOWN, INPUT);
 
     // activate backlight
     pinMode(9, OUTPUT);
     digitalWrite(9, HIGH);
 
+    Serial.println("Initizalizing TFT");
     // tft.init(240, 320);
     tft.begin();
+    Serial.println("Initizalizing TFT finished");
 
     pinMode(BUTTON_LEFT_UP, INPUT);
     pinMode(BUTTON_RIGHT_UP, INPUT);
@@ -24,25 +31,25 @@ void setup()
     digitalWrite(PIN_BUTTON_ONOFF_LED_RED, LOW);
     digitalWrite(PIN_BUTTON_ONOFF_LED_WHITE, HIGH);
 
+    Serial.println("Drawing rect");
     tft.fillScreen(ILI9341_WHITE);
-    tft.drawRect(10, 10, 100, 100, ILI9341_RED);
+    tft.fillRect(10, 10, 100, 100, ILI9341_RED);
+    tft.fillRect(10, 110, 100, 100, ILI9341_YELLOW);
+    Serial.println("Drawing rect finished");
     // tft.fillScreen(ST77XX_WHITE);
     // tft.drawRect(10, 10, 100, 100, ST77XX_RED);
-}
-
-String _serialString = "";
-void serialEvent()
-{
 }
 
 void loop(void)
 {
     // uint8_t btLeftUp = digitalRead(BUTTON_LEFT_UP); // not needed for now
+    uint8_t btLeftDown = digitalRead(BUTTON_LEFT_DOWN);
     uint8_t btRightUp = digitalRead(BUTTON_RIGHT_UP);
     uint8_t btRightDown = digitalRead(BUTTON_RIGHT_DOWN);
 
     if(btRightUp == HIGH)
     {
+        Serial.println("Button RightUp");
         // tft.fillScreen(ST77XX_WHITE);
         tft.fillScreen(ILI9341_WHITE);
         delay(1000);
@@ -59,7 +66,15 @@ void loop(void)
     // reset tft
     else if(btRightDown == HIGH)
     {
+        Serial.println("Button RightDown");
         tft.begin();
         // tft.init(240, 320);
+    }
+    else if(btLeftDown == HIGH)
+    {
+        Serial.println("Button LeftDown");
+        digitalWrite(PIN_POWER, LOW);
+        digitalWrite(PIN_BUTTON_ONOFF_LED_WHITE, LOW);
+        delay(5000);
     }
 }
--- src/main.h
+++ src/main.h
@@ -6,12 +6,16 @@
 // #include "Adafruit_ST7789.h" // Hardware-specific library for ST7789 (with or without CS pin)
 
 #define DELAY 3000
-#define BUTTON_PIN_RED 37
-#define BUTTON_PIN_WHITE 49
+
+#define PIN_POWER 25
+
+#define PIN_BUTTON_ONOFF_LED_WHITE 37
+#define PIN_BUTTON_ONOFF_LED_RED 49
 
 #define BUTTON_RIGHT_UP 30
-#define BUTTON_LEFT_UP 31
 #define BUTTON_RIGHT_DOWN 32
+#define BUTTON_LEFT_UP 31
+#define BUTTON_LEFT_DOWN 33
 
 #define TFT_RST 51
 #define TFT_DC 11
